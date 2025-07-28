c3350683fe8a757a30639a37713e629c6dcd011e
Ha Quanq <quangha0120010@gmail.com>
2025-07-28 08:52:29 +0700

    Update Vite build public path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root: "src/",
     build: { outDir: "../dist/", emptyOutDir: true },
-    base: "/fm-clipboard-landing-page/",
+    base: "/clipboard-landing-page/",
 });
